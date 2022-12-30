84eae33a9ef2e512d6e46ef9fa851d5a38738916
Derek Boucher <derek.boucher@me.com>
2022-12-29 20:02:29 -0500

    added register fields to cpu struct

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -1,3 +1,22 @@
-pub struct _LR35902 {}
+const ZERO_FLAG_MASK: u8 = 1 << 7;
+const SUB_FLAG_MASK: u8 = 1 << 6;
+const HALF_CARRY_FLAG_MASK: u8 = 1 << 5;
+const CARRY_FLAG_MASK: u8 = 1 << 4;
 
-impl _LR35902 {}
+union Register {
+    hi: u8,
+    lo: u8,
+    data: u16,
+}
+
+pub struct LR35902 {
+    af: Register,
+    bc: Register,
+    de: Register,
+    hl: Register,
+
+    sp: u16,
+    pc: u16,
+}
+
+impl LR35902 {}
