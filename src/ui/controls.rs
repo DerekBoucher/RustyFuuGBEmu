a5de3e19966bd52c605a3387c6665e90de7bdd7b
Derek Boucher <derek.a.boucher@gmail.com>
2024-08-10 13:10:44 -0400

    refactor: moved control input logic to the ui::controls module

--- src/ui/controls.rs
+++ src/ui/controls.rs
@@ -1,11 +1,28 @@
 use egui::{Context, RichText};
 use glium::glutin::event::{ElementState, VirtualKeyCode, WindowEvent};
 
+use crate::{
+    gameboy::channel::front_end::Frontend,
+    joypad::{ActionButton, DirectionButton},
+};
+
 pub struct Ui {
     show: bool,
     show_key_bind_window: bool,
     gb_control_png: &'static [u8],
-    pending_bind: *mut VirtualKeyCode,
+
+    // Mapped input keys
+    key_a: VirtualKeyCode,
+    key_b: VirtualKeyCode,
+    key_start: VirtualKeyCode,
+    key_select: VirtualKeyCode,
+    key_up: VirtualKeyCode,
+    key_down: VirtualKeyCode,
+    key_left: VirtualKeyCode,
+    key_right: VirtualKeyCode,
+
+    // Key to modify
+    key_to_modify: (Option<DirectionButton>, Option<ActionButton>),
 }
 
 impl Ui {
@@ -16,7 +33,17 @@ impl Ui {
             show: false,
             show_key_bind_window: false,
             gb_control_png,
-            pending_bind: std::ptr::null_mut(),
+
+            key_a: VirtualKeyCode::A,
+            key_b: VirtualKeyCode::S,
+            key_start: VirtualKeyCode::D,
+            key_select: VirtualKeyCode::F,
+            key_up: VirtualKeyCode::Up,
+            key_down: VirtualKeyCode::Down,
+            key_left: VirtualKeyCode::Left,
+            key_right: VirtualKeyCode::Right,
+
+            key_to_modify: (None, None),
         }
     }
 
@@ -24,18 +51,7 @@ impl Ui {
         self.show = show;
     }
 
-    pub fn render(
-        &mut self,
-        egui_ctx: &Context,
-        key_a: &mut VirtualKeyCode,
-        key_b: &mut VirtualKeyCode,
-        key_start: &mut VirtualKeyCode,
-        key_select: &mut VirtualKeyCode,
-        key_up: &mut VirtualKeyCode,
-        key_down: &mut VirtualKeyCode,
-        key_left: &mut VirtualKeyCode,
-        key_right: &mut VirtualKeyCode,
-    ) {
+    pub fn render(&mut self, egui_ctx: &Context) {
         self.render_key_bind_window(egui_ctx);
 
         if !self.show {
@@ -70,116 +86,122 @@ impl Ui {
                 ui.horizontal_centered(|ui| {
                     ui.vertical_centered_justified(|ui| {
                         ui.horizontal(|ui| {
-                            ui.label(RichText::new("Up -> ").size(15.0));
+                            ui.label(RichText::new("Up: ").size(15.0));
                             if ui
                                 .button(
-                                    RichText::new(format_args!("{:?}", &key_up).to_string())
+                                    RichText::new(format_args!("{:?}", &self.key_up).to_string())
                                         .size(15.0),
                                 )
                                 .clicked()
                             {
                                 self.show_key_bind_window = true;
-                                self.pending_bind = key_up;
+                                self.key_to_modify = (Some(DirectionButton::Up), None);
                             }
                         });
 
                         ui.horizontal(|ui| {
-                            ui.label(RichText::new("Down -> ").size(15.0));
+                            ui.label(RichText::new("Down: ").size(15.0));
                             if ui
                                 .button(
-                                    RichText::new(format_args!("{:?}", &key_down).to_string())
+                                    RichText::new(format_args!("{:?}", &self.key_down).to_string())
                                         .size(15.0),
                                 )
                                 .clicked()
                             {
                                 self.show_key_bind_window = true;
-                                self.pending_bind = key_down;
+                                self.key_to_modify = (Some(DirectionButton::Down), None);
                             }
                         });
 
                         ui.horizontal(|ui| {
-                            ui.label(RichText::new("Left -> ").size(15.0));
+                            ui.label(RichText::new("Left: ").size(15.0));
                             if ui
                                 .button(
-                                    RichText::new(format_args!("{:?}", &key_left).to_string())
+                                    RichText::new(format_args!("{:?}", &self.key_left).to_string())
                                         .size(15.0),
                                 )
                                 .clicked()
                             {
                                 self.show_key_bind_window = true;
-                                self.pending_bind = key_left;
+                                self.key_to_modify = (Some(DirectionButton::Left), None);
                             }
                         });
 
                         ui.horizontal(|ui| {
-                            ui.label(RichText::new("Right -> ").size(15.0));
+                            ui.label(RichText::new("Right: ").size(15.0));
                             if ui
                                 .button(
-                                    RichText::new(format_args!("{:?}", &key_right).to_string())
-                                        .size(15.0),
+                                    RichText::new(
+                                        format_args!("{:?}", &self.key_right).to_string(),
+                                    )
+                                    .size(15.0),
                                 )
                                 .clicked()
                             {
                                 self.show_key_bind_window = true;
-                                self.pending_bind = key_right;
+                                self.key_to_modify = (Some(DirectionButton::Right), None);
                             }
                         });
                     });
 
                     ui.vertical_centered_justified(|ui| {
                         ui.horizontal(|ui| {
-                            ui.label(RichText::new("A -> ").size(15.0));
+                            ui.label(RichText::new("A: ").size(15.0));
                             if ui
                                 .button(
-                                    RichText::new(format_args!("{:?}", &key_a).to_string())
+                                    RichText::new(format_args!("{:?}", &self.key_a).to_string())
                                         .size(15.0),
                                 )
                                 .clicked()
                             {
                                 self.show_key_bind_window = true;
-                                self.pending_bind = key_a;
+                                self.key_to_modify = (None, Some(ActionButton::A));
                             }
                         });
 
                         ui.horizontal(|ui| {
-                            ui.label(RichText::new("B -> ").size(15.0));
+                            ui.label(RichText::new("B: ").size(15.0));
                             if ui
                                 .button(
-                                    RichText::new(format_args!("{:?}", &key_b).to_string())
+                                    RichText::new(format_args!("{:?}", &self.key_b).to_string())
                                         .size(15.0),
                                 )
                                 .clicked()
                             {
                                 self.show_key_bind_window = true;
-                                self.pending_bind = key_b;
+                                self.key_to_modify = (None, Some(ActionButton::B));
                             }
                         });
 
                         ui.horizontal(|ui| {
-                            ui.label(RichText::new("Start -> ").size(15.0));
+                            ui.label(RichText::new("Start: ").size(15.0));
                             if ui
                                 .button(
-                                    RichText::new(format_args!("{:?}", &key_start).to_string())
-                                        .size(15.0),
+                                    RichText::new(
+                                        format_args!("{:?}", &self.key_start).to_string(),
+                                    )
+                                    .size(15.0),
                                 )
                                 .clicked()
                             {
                                 self.show_key_bind_window = true;
-                                self.pending_bind = key_start;
+                                self.key_to_modify = (None, Some(ActionButton::Start));
                             }
                         });
 
                         ui.horizontal(|ui| {
-                            ui.label(RichText::new("Select -> ").size(15.0));
+                            ui.label(RichText::new("Select: ").size(15.0));
                             if ui
                                 .button(
-                                    RichText::new(format_args!("{:?}", &key_select).to_string())
-                                        .size(15.0),
+                                    RichText::new(
+                                        format_args!("{:?}", &self.key_select).to_string(),
+                                    )
+                                    .size(15.0),
                                 )
                                 .clicked()
                             {
                                 self.show_key_bind_window = true;
-                                self.pending_bind = key_select;
+                                self.key_to_modify = (None, Some(ActionButton::Select));
                             }
                         });
                     });
@@ -188,9 +210,7 @@ impl Ui {
     }
 
     fn render_key_bind_window(&mut self, egui_ctx: &Context) {
-        if !self.show_key_bind_window {
-            self.pending_bind = std::ptr::null_mut();
-        }
+        if !self.show_key_bind_window {}
 
         egui::Window::new("")
             .open(&mut self.show_key_bind_window)
@@ -202,7 +222,7 @@ impl Ui {
             });
     }
 
-    pub fn process_window_event(&mut self, event: WindowEvent<'_>) {
+    pub fn process_window_event(&mut self, event: WindowEvent<'_>, frontend: &Frontend) {
         if self.show_key_bind_window {
             match event {
                 WindowEvent::KeyboardInput { input, .. } => {
@@ -210,19 +230,83 @@ impl Ui {
                         return;
                     }
 
-                    if self.pending_bind.is_null() {
-                        return;
-                    }
-
                     if let Some(key) = input.virtual_keycode {
-                        unsafe {
-                            *self.pending_bind = key;
-                        }
                         self.show_key_bind_window = false;
+
+                        match self.key_to_modify {
+                            (Some(direction), None) => match direction {
+                                DirectionButton::Up => self.key_up = key,
+                                DirectionButton::Down => self.key_down = key,
+                                DirectionButton::Left => self.key_left = key,
+                                DirectionButton::Right => self.key_right = key,
+                            },
+                            (None, Some(action)) => match action {
+                                ActionButton::A => self.key_a = key,
+                                ActionButton::B => self.key_b = key,
+                                ActionButton::Start => self.key_start = key,
+                                ActionButton::Select => self.key_select = key,
+                            },
+                            _ => {}
+                        }
+
+                        self.key_to_modify = (None, None);
                     }
                 }
                 _ => {}
             }
+
+            return;
+        }
+
+        // Else we are processing actual gameplay inputs
+        match event {
+            WindowEvent::KeyboardInput { input, .. } => {
+                if input.state != ElementState::Pressed {
+                    return;
+                }
+
+                let key_pressed = input.virtual_keycode.unwrap();
+
+                if key_pressed == self.key_a {
+                    frontend.send_joypad_data(None, Some(ActionButton::A));
+                }
+
+                if key_pressed == self.key_b {
+                    frontend.send_joypad_data(None, Some(ActionButton::B));
+                }
+
+                if key_pressed == self.key_start {
+                    frontend.send_joypad_data(None, Some(ActionButton::Start));
+                }
+
+                if key_pressed == self.key_select {
+                    frontend.send_joypad_data(None, Some(ActionButton::Select));
+                }
+
+                if key_pressed == self.key_up {
+                    frontend.send_joypad_data(Some(DirectionButton::Up), None);
+                }
+
+                if key_pressed == self.key_down {
+                    frontend.send_joypad_data(Some(DirectionButton::Down), None);
+                }
+
+                if key_pressed == self.key_left {
+                    frontend.send_joypad_data(Some(DirectionButton::Left), None);
+                }
+
+                if key_pressed == self.key_right {
+                    frontend.send_joypad_data(Some(DirectionButton::Right), None);
+                }
+
+                log::trace!(
+                    "key scancode: {:?}, state: {:?}, virt: {:?}",
+                    input.scancode,
+                    input.state,
+                    input.virtual_keycode.unwrap()
+                );
+            }
+            _ => {}
         }
     }
 }
